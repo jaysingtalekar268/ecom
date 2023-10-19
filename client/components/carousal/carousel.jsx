71d1cacd2867b26f1a63e3e8beaeaba510299b21
jaysingtalekar268 <92983089+jaysingtalekar268@users.noreply.github.com>
2023-10-19 22:19:39 +0530

    added time for carousal

--- client/components/carousal/carousel.jsx
+++ client/components/carousal/carousel.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Image from "next/image"
 import "../../node_modules/bootstrap/dist/css/bootstrap.css"
@@ -7,58 +7,73 @@ import { BiChevronLeft, BiChevronRight } from "react-icons/bi"
 
 const CustomCarousal = ({ carousalItems }) => {
   const [carousalCurrentIndex, setCarousalCurrentIndex] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
 
-  const handlecarousalAutoIndexUpdate = () => {
-    setInterval(() => {
-      // while (carousalCurrentIndex < carousalItems.length) {
-      //   setTimeout(() => {
-      //     setCarousalCurrentIndex(carousalCurrentIndex + 1);
-      //   })
-      // })
-  }, 3000)
-}
-  , 3000 * carousalItems.length)
-}
+  useEffect(() => {
+    const id = handlecarousalAutoIndexUpdate(true);
+    return () => {
+      if (id) {
+        clearInterval(id);
+      }
+    };
+  }, []); // Empty dependency array to run the effect only once
 
-const handleCarousalIndex = (currentIndex) => {
-  if (currentIndex <= 0)
-    currentIndex = 0;
-  else if (currentIndex >= carousalItems.length)
-    currentIndex = carousalItems.length - 1;
+  const handlecarousalAutoIndexUpdate = (goForward) => {
+    const id = setInterval(() => {
+      setCarousalCurrentIndex(prevIndex => {
+        let newIndex = prevIndex + 1;
+        if (newIndex >= carousalItems.length) {
+          newIndex = 0; // Reset to the first item when reaching the end
+        }
+        return newIndex;
+      });
+    }, 1000 * 5);
 
-  setCarousalCurrentIndex(currentIndex);
-}
+    return id;
+  }
 
-return (
-  <div className={carousalStyle.carousal}>
-    <div className={carousalStyle.arrow_div}>
-      <BiChevronLeft className={carousalStyle.carousal_arrow}
-        onClick={() => handleCarousalIndex(carousalCurrentIndex - 1)}
-      />
-    </div>
-
-    {
-      carousalItems.map((item, index) => {
-        return (
-          <>
-            {index == carousalCurrentIndex &&
-              <div className={carousalStyle.carousal_item} key={index}>
-                <Image className={carousalStyle.carousal_image} src={item.image} alt={`${index} Image`}></Image>
-                {/* <span className={carousalStyle.carousal_text}>{item.text}</span> */}
-              </div>
-            }
-          </>
-
-        )
-      })
+  const handleCarousalIndex = (currentIndex) => {
+    if (currentIndex < 0) {
+      currentIndex = carousalItems.length - 1;
+    } else if (currentIndex >= carousalItems.length) {
+      currentIndex = 0;
     }
-    <div className={carousalStyle.arrow_div}>
-      <BiChevronRight className={carousalStyle.carousal_arrow}
-        onClick={() => handleCarousalIndex(carousalCurrentIndex + 1)}
-      />
-    </div>
-  </div >
-);
+
+    setCarousalCurrentIndex(currentIndex);
+  }
+
+  // ... rest of your component code
+
+  return (
+    <div className={carousalStyle.carousal}>
+      <div className={carousalStyle.arrow_div}>
+        <BiChevronLeft className={carousalStyle.carousal_arrow}
+          onClick={() => handleCarousalIndex(carousalCurrentIndex - 1)}
+        />
+      </div>
+
+      {
+        carousalItems.map((item, index) => {
+          return (
+            <>
+              {index == carousalCurrentIndex &&
+                <div className={carousalStyle.carousal_item} key={index}>
+                  <Image className={carousalStyle.carousal_image} src={item.image} alt={`${index} Image`}></Image>
+                  {/* <span className={carousalStyle.carousal_text}>{item.text}</span> */}
+                </div>
+              }
+            </>
+
+          )
+        })
+      }
+      <div className={carousalStyle.arrow_div}>
+        <BiChevronRight className={carousalStyle.carousal_arrow}
+          onClick={() => handleCarousalIndex(carousalCurrentIndex + 1)}
+        />
+      </div>
+    </div >
+  );
 
 }
 
